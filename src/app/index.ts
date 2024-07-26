1fa919e50c97730dd5b5c9efa97959074d5e9efe
rbanning <rob@myhallpass.com>
2024-07-25 18:23:18 -0700

    setting up for deployment

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,13 @@
 import '../style.css'
+import academyLogo from '../assets/academy-logo.svg';
 import hallpassLogo from '../assets/hallpass.png';
 import viteLogo from '../assets/vite.svg';
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${hallpassLogo}" class="logo" alt="Hallpass logo" />
+    <a href="https://academy.hallpassandfriends.com" target="_blank">
+      <img src="${academyLogo}" class="logo" alt="Hallpass Academy logo" />
     </a>
     <h1>Advanced Typescript Series</h1>
     <div class="card">
@@ -16,8 +17,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Getting Started
     </p>
     <footer>
-      <img src="${viteLogo}" class="logo mini vanilla" alt="Vite logo" />
-      <a href="https://vitejs.dev" target="_blank">Powered by Vite</a>
+      <section>
+        <img src="${hallpassLogo}" class="logo mini vanilla" alt="Hallpass logo" />
+        <a href="https://academy.hallpassandfriends.com" target="_blank">Created by Hallpass</a>
+      </section>
+      <section>
+        <img src="${viteLogo}" class="logo mini vanilla" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank">Powered by Vite</a>
+      </section>
   </div>
 `
 
